9781205546665426c276e04c6402aca5d9e15ceb
saurabh1198 <42364870+saurabh1198@users.noreply.github.com>
2018-11-13 08:16:24 -0800

    2d transformation about arbitary point

--- 2d transformation.cpp
+++ 2d transformation.cpp
@@ -1,88 +1,91 @@
 #include<iostream.h>
-#include<conio.h>
 #include<graphics.h>
+#include<stdio.h>
+#include<dos.h>
 #include<math.h>
-void rotation(float x1,float y1,float x2,float y2,float x3,float y3)
+#include<conio.h>
+
+int xmax,ymax,xmid,ymid,o1,o2,ang;
+float rang;
+int x1,x2,x3,y1,y2,y3,x,y;
+
+void drawtri(int x1,int x2,int x3,int y1,int y2,int y3)
 {
-	float t;
-	cout<<"\t\t Rotation"<<"\n";
-	cout<<"Enter angle of Rotation"<<endl;
-	cin>>t;
-	t=t*(3.14/180);
-	x1=x1*cos(t)-y1*sin(t);
-	y1=x1*sin(t)+y1*cos(t);
-	x2=x2*cos(t)-y2*sin(t);
-	y2=x2*sin(t)+y2*cos(t);
-	x3=x3*cos(t)-y3*sin(t);
-	y3=x3*sin(t)+y3*cos(t);
 
-	line(x1,y1,x2,y2);
-	line(x2,y2,x3,y3);
-	line(x3,y3,x1,y1);
+  line(x1+xmid,ymid-y1,xmid+x2,ymid-y2);
+  line(x1+xmid,ymid-y1,xmid+x3,ymid-y3);
+  line(x3+xmid,ymid-y3,xmid+x2,ymid-y2);
 }
-void scaling(float x1,float y1,float x2,float y2,float x3,float y3)
+
+
+void ref()
 {
-	int sx,sy;
-	cout<<"\t\t Scaling"<<"\n";
-	cout<<"Enter co-ordinates for Scaling"<<endl;
-	cin>>sx>>sy;
-	x1=x1*sx;
-	y1=y1*sy;
-	x2=x2*sx;
-	y2=y2*sy;
-	x3=x3*sx;
-	y3=y3*sy;
+  cout<<"\n Enter coordinates of arbitrary point: "<<endl;
+  cin>>x>>y;
 
-	line(x1,y1,x2,y2);
-	line(x2,y2,x3,y3);
-	line(x3,y3,x1,y1);
+  xmid=xmid+x;
+  ymid=ymid-y;
+  x1=-x1;
+  y1=-y1;
+  x2=-x2;
+  y2=-y2;
+  x3=-x3;
+  y3=-y3;
+  sleep(2);
+  drawtri(x1+x,x2+x,x3+x,y1-y,y2-y,y3-y);
+  goto label;
+ }
+
+void rot()
+ {
+  cout<<"Rotation\n";
+  cout<<"\n Enter coordinates of arbitrary point: "<<endl;
+  cin>>x>>y;
+  putpixel(x,y,15);
+  cout<<"\n Enter angle: ";
+  cin>>ang;
+
+  rang=((ang*3.14)/180.0);
+
+  xmid=xmid+x;
+  ymid=ymid-y;
+
+  x1=x1*cos(rang)- y1*sin(rang);
+  y1=x1*sin(rang)+ y1*cos(rang);
+  x2=x2*cos(rang)- y2*sin(rang);
+  y2=x2*sin(rang)+ y2*cos(rang);
+  x3=x3*cos(rang)- y3*sin(rang);
+  y3=x3*sin(rang)+ y3*cos(rang);
+
+  sleep(2);
+  drawtri(x1,x2,x3,y1,y2,y3);
 }
-void translation(float x1,float y1,float x2,float y2,float x3,float y3)
+void main()
 {
-	int tx,ty;
-	cout<<"\t\t Translation"<<"\n";
-	cout<<"Enter co-ordinates for Translation"<<endl;
-	cin>>tx>>ty;
-	x1=x1+tx;
-	y1=y1+ty;
-	x2=x2+tx;
-	y2=y2+ty;
-	x3=x3+tx;
-	y3=y3+ty;
 
-	line(x1,y1,x2,y2);
-	line(x2,y2,x3,y3);
-	line(x3,y3,x1,y1);
-}
-void main(){
-	clrscr();
-	int gd=DETECT,gm ;
-	float x1,x2,y1,y2,x3,y3;
-	initgraph(&gd,&gm,"C:\\TURBOC3\\BGI");
-	cout<<"\t\t"<<"Transformation"<<"\n\n";
-	x1=200;
-	y1=200;
-	x2=250;
-	y2=150;
-	x3=300;
-	y3=200;
-	line(x1,y1,x2,y2);
-	line(x2,y2,x3,y3);
-	line(x3,y3,x1,y1);
+ cout<<"x1 y1: ";
+ cin>>x1>>y1;
+ cout<<"x2 y2: ";
+ cin>>x2>>y2;
+ cout<<"x3 y3: ";
+ cin>>x3>>y3;
+ int gd=DETECT,gm;
+ initgraph(&gd,&gm,"C://TURBOC3//BGI");
 
-	int ch;
-	cout<<"Enter Choice: "<<"\n"<<" 1: Rotation"<<"\n"<<" 2: Translation"<<"\n"<<" 3: Scaling"<<endl;
-	cin>>ch;
-	switch(ch)	{
-		case 1:
-			rotation(x1,y1,x2,y2,x3,y3);
-			break;
-		case 2:
-			translation(x1,y1,x2,y2,x3,y3);
-			break;
-		case 3:
-			scaling(x1,y1,x2,y2,x3,y3);
-			break;
-	}
-	getch();
+ xmax=getmaxx();
+ ymax=getmaxy();
+ xmid=xmax/2;
+ ymid=ymax/2;
+
+   for(int i=0;i<xmax;i++)
+     putpixel(i,ymid+y,YELLOW);
+   for(i=0;i<ymax;i++)
+     putpixel(xmid-x,i,YELLOW);
+
+  drawtri(x1,x2,x3,y1,y2,y3);
+
+  ref();
+  rot();
+  getch();
 }
+
